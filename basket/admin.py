e445072ba0c33f51ac397c43201d5394d9ba0b68
Francisco Diaz <Panxo@MacBook-de-Francisco.local>
2018-06-01 17:17:25 -0300

    v2

--- basket/admin.py
+++ basket/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from basket.models import Equipo
 
 # Register your models here.
+
+@admin.register(Equipo)
+class EquipoAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'thumbnail',)
--- basket/models.py
+++ basket/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
 # Create your models here.
+
+class Equipo(models.Model):
+	nombre = models.CharField(max_length = 50)
+	descripcion = models.TextField()
+	logo = models.ImageField(upload_to = 'logo_equipo')
+
+
+	def thumbnail(self):
+		if self.logo:
+			return mark_safe('<img src="%s" widght=80 height=80/>' % self.logo.url)
+		else:
+			return '(no imagen)'
+		thumbnail.short_description = 'Thumb'
+
+
+	def __str__(self):
+		return self.nombre
