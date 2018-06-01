59c2327360b2ad8603a85eb4b4b0f766e2a7a13d
Francisco Diaz <Panxo@MacBook-de-Francisco.local>
2018-06-01 19:01:37 -0300

    v3

--- basket/admin.py
+++ basket/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from basket.models import Equipo
+from basket.models import Equipo, Jugador, Entrenador
 
 # Register your models here.
 
 @admin.register(Equipo)
 class EquipoAdmin(admin.ModelAdmin):
 	list_display = ('nombre', 'thumbnail',)
+
+
+@admin.register(Jugador)
+class JugadorAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'apodo', 'edad', 'thumbnail',)
+
+
+@admin.register(Entrenador)
+class EntrenadorAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'apodo', 'edad', 'email',)
--- basket/models.py
+++ basket/models.py
@@ -19,3 +19,51 @@ class Equipo(models.Model):
 
 	def __str__(self):
 		return self.nombre
+
+
+class Jugador(models.Model):
+	nombre = models.CharField(max_length = 50)
+	apodo = models.CharField(max_length = 50)
+	nacimiento = models.DateField()
+	edad = models.PositiveIntegerField()
+	email = models.EmailField()
+	estatura = models.PositiveIntegerField(help_text = 'Indique estatura en centimetros')
+	peso = models.PositiveIntegerField(help_text = 'Indique peso en libras')
+	rut = models.CharField(max_length = 8)
+	digito_verificador = models.PositiveIntegerField(help_text = 'Si el digito verificador termina en K, reemplace con un 0')
+	foto = models.ImageField(upload_to = 'foto_jugador')
+	equipo = models.ForeignKey('Equipo', null = True, blank = True, on_delete = models.CASCADE)
+	
+
+	def thumbnail(self):
+		if self.foto:
+			return mark_safe('<img src="%s" widght=80 height=80/>' % self.foto.url)
+		else:
+			return '(no imagen)'
+		thumbnail.short_description = 'Thumb'
+
+
+	def fullRut(self):
+		return '{}-{}' . format(self.rut, self.digito_verificador)
+
+
+	def __str__(self):
+		return self.nombre
+
+
+class Entrenador(models.Model):
+	nombre = models.CharField(max_length = 50)
+	apodo = models.CharField(max_length = 50)
+	edad = models.PositiveIntegerField()
+	email = models.EmailField()
+	rut = models.CharField(max_length = 8)
+	digito_verificador = models.PositiveIntegerField(help_text = 'Si el digito verificador termina en k, reemplace con un 0')
+	equipo = models.OneToOneField('Equipo', null = True, blank = True, on_delete = models.CASCADE)
+
+
+	def fullRut(self):
+		return '{}-{}' . format(self.rut, self.digito_verificador)
+
+
+	def __str__(self):
+		return self.nombre
